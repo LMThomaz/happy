808fc9e443e706f38e13438d041ccf7d407a2941
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-10-15 23:51:09 -0300

    implements front-end web in project Happy

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -5,7 +5,10 @@ export default {
   storage: multer.diskStorage({
     destination: path.join(__dirname, '..', '..', 'uploads'),
     filename: (request, file, cb) => {
-      const fileName = `${Date.now()}-${file.originalname}`;
+      const fileName = `${Date.now()}-${file.originalname}`.replace(
+        /[^a-zA-Z0-9]/g,
+        '_'
+      );
 
       cb(null, fileName);
     },
